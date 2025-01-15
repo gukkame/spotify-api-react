6555eb8ef116913911d1ce5861e80672ad29ab67
gukkame <guka245@gmail.com>
2025-01-15 18:24:56 +0200

    Added defind host and port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all IPs
+    port: process.env.PORT || 8080, // Use Railway's PORT or default to 3000
+  },
 })
